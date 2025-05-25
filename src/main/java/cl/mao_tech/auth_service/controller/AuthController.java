85d3be56b87226f9f1b49f6f9dbbae54753ef004
o-aguirre <two.aguirre@gmail.com>
2025-05-24 20:12:41 -0400

    ADD: Conectar

---
+++ src/main/java/cl/mao_tech/auth_service/controller/AuthController.java
@@ -0,0 +1,59 @@
+package cl.mao_tech.auth_service.controller;
+
+import cl.mao_tech.auth_service.model.AuthenticationRequest;
+import cl.mao_tech.auth_service.model.User;
+import cl.mao_tech.auth_service.repository.UserRepository;
+import cl.mao_tech.auth_service.service.UserService;
+import cl.mao_tech.auth_service.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class AuthController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user) {
+        // Encode the user's password
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        // Save the user to the database
+        userRepository.save(user);
+        return "User registered successfully";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(authenticationRequest.getMail(), authenticationRequest.getPassword())
+        );
+
+        final UserDetails userDetails = userService.loadUserByUsername(authenticationRequest.getMail());
+        final String jwt = jwtUtil.generateToken(userDetails);
+
+        return jwt;
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+}
